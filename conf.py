a30a83e6023342deb125b5b03cc387ee406f254d
Anurag Upadhyay <iitg.anurag@gmail.com>
2022-05-14 19:39:23 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -119,19 +119,19 @@ htmlhelp_basename = 'vfoDocs'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-    The paper size ('letterpaper' or 'a4paper').
+    # The paper size ('letterpaper' or 'a4paper').
     
     'papersize': 'letterpaper',
 
-    The font size ('10pt', '11pt' or '12pt').
+    # The font size ('10pt', '11pt' or '12pt').
     
     'pointsize': '10pt',
 
-    Additional stuff for the LaTeX preamble.
+    # Additional stuff for the LaTeX preamble.
     
     'preamble': '',
 
-    Latex figure (float) alignment
+    # Latex figure (float) alignment
     
     'figure_align': 'htbp',
 }
